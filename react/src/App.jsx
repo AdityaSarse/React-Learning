85917d6e0ea3a999a6d58b196616bd86b994443a
AdityaSarse <adityasarse362@gmail.com>
2026-01-08 11:16:06 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div id="parent-1">
+        <h1>App1</h1>
+      </div>
+      <div id="parent-2">
+        <h1>App2</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
